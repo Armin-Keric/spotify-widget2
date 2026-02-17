27b721146657e9923a91c8aa0a65a56c0c8b86a6
LeBonsBay <sasuek94@gmail.com>
2026-02-17 23:58:36 +0100

    -

---
+++ src/utils.ts
@@ -0,0 +1,26 @@
+import type {LastFMInterface} from "./LastFMInterface.ts";
+import {data} from "./last.fm.ts";
+
+const musicData:LastFMInterface = data;
+
+//dont forget the notlistening case...
+export function getAlbum():string{
+    const albumName: string = musicData.album?.["#text"] ?? "null";
+    return albumName;
+}
+
+export function getArtist():string{
+    const artistName:string = musicData.artist?.["#text"] ?? "null";
+    return artistName;
+}
+export function getNowPlaying():boolean{
+    const isPlaying = musicData["@attr"]?.nowplaying === "true"
+    return isPlaying.valueOf();
+}
+export function getImage():string{
+    const imageURL:string = musicData.image[3]?.["#text"] ?? "https://via.placeholder.com/300"
+    return imageURL;
+}
+export function getSongName():string{
+    return musicData?.name ?? "null"
+}
