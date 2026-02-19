ef29c7578e69c7063af87727eb53eadb27c01640
LeBone <sasuek94@gmail.com>
2026-02-19 19:24:28 +0100

    Delete src directory

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import {generateSVG} from "./render.ts";
-import {getAlbum, getArtist, getImage, getNowPlaying, getSongName} from "./utils.ts";
-
-console.log(getAlbum());
-console.log(getSongName());
-console.log(getArtist());
-console.log(getNowPlaying());
-console.log(getImage());
-console.log(generateSVG())
